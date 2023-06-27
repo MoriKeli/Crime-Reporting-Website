9aa2648aded1c93a35a0b8802155665fa3eab366
brocode <kelimbth@gmail.com>
2023-06-27 09:31:42 +0300

    added docstring & overrode save()

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 class User(AbstractUser):
+    """ This model maps and stores common user profile attributes. The model extends from AbstractUser class. """
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=7, blank=False)
@@ -16,7 +18,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-    
+
+    # override save() to resize user's profile picture.
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+
+        img = Image.open(self.profile_pic.path)
+        if img.height > 400 and img.width > 400:
+            output_size = (400, 400)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
     class Meta:
         ordering = ['first_name', 'last_name', '-created']
