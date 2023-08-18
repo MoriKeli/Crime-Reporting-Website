48ab1077a5ae9a3738c05fd12bbef5d4d3cbfbea
MoriKeli <kelimbth@gmail.com>
2023-08-18 08:27:04 +0300

    overrode ready()

--- users/reporters/apps.py
+++ users/reporters/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ReportersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reporters'
+
+    def ready(self):
+        import users.reporters.signals
