b7e776b5b91c78ff391936c8ceb221ac40dcf041
MoriKeli <kelimbth@gmail.com>
2023-08-18 08:25:49 +0300

    updated INSTALLED_APPS registered new app

--- src/settings.py
+++ src/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
