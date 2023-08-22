6238ea8d4fbf32c1b754d061b7ef8a9036b4adfa
MoriKeli <kelimbth@gmail.com>
2023-08-22 21:38:47 +0300

    deleted signal func. deleted generate_officerID()

--- users/officers/signals.py
+++ users/officers/signals.py
@@ -1,15 +1,10 @@
-from .models import PolicePosts, OfficerProfile, SuspectsRecords
+from .models import PolicePosts, SuspectsRecords
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from uuid import uuid4
 
 @receiver(pre_save, sender=PolicePosts)
 def generate_policepostID(sender, instance, **kwargs):
-    if instance.id == '':
-        instance.id = str(uuid4()).replace('-', '')[:25]
-
-@receiver(pre_save, sender=OfficerProfile)
-def generate_officerID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid4()).replace('-', '')[:25]
 
