f556fb51598edf247b60780a69c9f0bf7ce9248a
MoriKeli <kelimbth@gmail.com>
2023-08-22 21:10:12 +0300

    overrode ready() overriding ready method whenever a signal function is called

--- users/officers/apps.py
+++ users/officers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OfficersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'officers'
+
+    def ready(self):
+        import users.officers.signals
